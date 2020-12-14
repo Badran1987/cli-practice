1e9b1ac6fb024e0c7f5b4208e9227d38a0373777
Badran1987 <52219058+Badran1987@users.noreply.github.com>
2020-12-14 15:28:57 -0500

    add calcolatConversion function Test

--- test/index.test.js
+++ test/index.test.js
@@ -10,9 +10,11 @@
 // file. In this case we've defined the function and the corresponding test in the
 // same file for illustrative and learning purposes.
 
-//validateUserInput function Test
-var { validateUserInput } = require('../src/validate-user-input');
 
+var { validateUserInput } = require('../src/validate-user-input');
+var { calcolatConversion } = require('../src/CalculateConversion');
+
+//validateUserInput function Test
 describe('validateUserInput', function() {
 
   test('should Validate User input is correct and not undefined ', function() {
@@ -20,6 +22,34 @@ describe('validateUserInput', function() {
     var result = validateUserInput();
 
     expect(result).toBe('You enter the Amount, Main Currency or Targit Currency Undefined');
+  });
+
+  
+});
+//calcolatConversion function Test
+describe('calcolatConversion', function() {
+
+  test('should Validate User input is correct and not undefined ', function() {
+    var rates = {
+
+      CAD: {
+       USD:0.784142,
+       JOD:0.554142,
+      },
+      USD: {
+       CAD: 1.27528,
+       JOD: 1.22,
+      },
+      JOD: {
+          CAD: 1.27528,
+          USD: 1.27528,
+         },
+      };
+      var mainCurrncy = 'CAD';
+      var targitCurrancy = 'USD';
+ 
+    var result = calcolatConversion(rates,mainCurrncy,targitCurrancy);
+    expect(result).toBe(0.784142);
   });
 
   
